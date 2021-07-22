4dacfc625ca4da361081413b3e75092749065ae4
Maria Salovskaya <salovskaya-m@yandex.ru>
2021-07-22 23:16:19 +0300

    ДЗ 2: Контейнеры и алгоритмы. Удаление дублирования кода

--- hw2-generics/src/main/java/homework/CustomerService.java
+++ hw2-generics/src/main/java/homework/CustomerService.java
@@ -13,26 +13,22 @@ public class CustomerService {
 
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        Map.Entry<Customer, String> entry = map.firstEntry();
-        Customer customer = entry.getKey();
-        return new Entry<>(
-            new Customer(customer.getId(), customer.getName(), customer.getScores()),
-            entry.getValue());
+        return cloneEntity(map.firstEntry());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
         Optional<Map.Entry<Customer, String>> entry = Optional.ofNullable(map.higherEntry(customer));
-        if (entry.isPresent()) {
-            Customer prevCustomer = entry.get().getKey();
-            return new Entry<>(
-                    new Customer(prevCustomer.getId(), prevCustomer.getName(), prevCustomer.getScores()),
-                    entry.get().getValue());
-        }
-        return null;
+        return entry.map(this::cloneEntity).orElse(null);
     }
 
     public void add(Customer customer, String data) {
         map.put(customer, data);
+    }
+
+    private Map.Entry<Customer, String> cloneEntity(Map.Entry<Customer, String> entry) {
+        Customer customer = entry.getKey();
+        return new Entry<>(new Customer(customer.getId(), customer.getName(), customer.getScores()),
+                            entry.getValue());
     }
 
     static final class Entry<Customer, String> implements Map.Entry<Customer, String> {
