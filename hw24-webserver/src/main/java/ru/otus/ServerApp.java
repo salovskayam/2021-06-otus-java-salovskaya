0f0807b4677b864733461cb9987328ecd81ee5f2
masha <salovskaya-m@yandex.ru>
2021-11-12 22:09:34 +0300

    hw24-webserver: ДЗ Веб сервер. Fix

---
+++ hw24-webserver/src/main/java/ru/otus/ServerApp.java
@@ -0,0 +1,63 @@
+package ru.otus;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.eclipse.jetty.security.HashLoginService;
+import org.eclipse.jetty.security.LoginService;
+import org.hibernate.cfg.Configuration;
+import ru.otus.core.repository.DataTemplateHibernate;
+import ru.otus.core.repository.HibernateUtils;
+import ru.otus.core.sessionmanager.TransactionManagerHibernate;
+import ru.otus.crm.dbmigrations.MigrationsExecutorFlyway;
+import ru.otus.crm.model.Address;
+import ru.otus.crm.model.Client;
+import ru.otus.crm.model.Phone;
+import ru.otus.crm.service.DBServiceClient;
+import ru.otus.crm.service.DbServiceClientImpl;
+import ru.otus.helpers.FileSystemHelper;
+import ru.otus.server.UsersWebServer;
+import ru.otus.server.UsersWebServerImpl;
+import ru.otus.services.TemplateProcessor;
+import ru.otus.services.TemplateProcessorImpl;
+
+public class ServerApp {
+    public static final String HIBERNATE_CFG_FILE = "hibernate.cfg.xml";
+
+    private static final int WEB_SERVER_PORT = 8080;
+    private static final String TEMPLATES_DIR = "/templates/";
+
+    private static final String HASH_LOGIN_SERVICE_CONFIG_NAME = "realm.properties";
+    private static final String REALM_NAME = "AnyRealm";
+
+    public static void main(String[] args) throws Exception {
+        var configuration = new Configuration().configure(HIBERNATE_CFG_FILE);
+
+        var dbUrl = configuration.getProperty("hibernate.connection.url");
+        var dbUserName = configuration.getProperty("hibernate.connection.username");
+        var dbPassword = configuration.getProperty("hibernate.connection.password");
+
+        new MigrationsExecutorFlyway(dbUrl, dbUserName, dbPassword).executeMigrations();
+
+        var sessionFactory = HibernateUtils.buildSessionFactory(configuration, Client.class, Address.class, Phone.class);
+
+        var transactionManager = new TransactionManagerHibernate(sessionFactory);
+///
+        var clientTemplate = new DataTemplateHibernate<>(Client.class);
+///
+        DBServiceClient dbServiceClient = new DbServiceClientImpl(transactionManager, clientTemplate);
+
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation()
+                .serializeNulls().setPrettyPrinting().create();
+
+        TemplateProcessor templateProcessor = new TemplateProcessorImpl(TEMPLATES_DIR);
+
+        String hashLoginServiceConfigPath = FileSystemHelper.localFileNameOrResourceNameToFullPath(HASH_LOGIN_SERVICE_CONFIG_NAME);
+        LoginService loginService = new HashLoginService(REALM_NAME, hashLoginServiceConfigPath);
+
+        UsersWebServer server = new UsersWebServerImpl(WEB_SERVER_PORT, gson, templateProcessor,
+                loginService, dbServiceClient);
+
+        server.start();
+        server.join();
+    }
+}
